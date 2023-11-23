52df45df31e5b887db790d1c0aa7ce25b28c378b
Ribeiro <arrb@gft.com>
2023-11-22 21:56:05 -0300

    .

--- Repository/tests/Pattern.Repository.Unit.Tests/Repositorys/MyDbContext.cs
+++
@@ -1,7 +0,0 @@
-namespace Pattern.Repository.Unit.Tests.Repositorys
-{
-    public class MyDbContext : DbContext
-    {
-        public virtual DbSet<Person> Persons { get; set; }
-    }
-}
--- Repository/tests/Pattern.Repository.Unit.Tests/Repositorys/Person.cs
+++
@@ -1,7 +0,0 @@
-namespace Pattern.Repository.Unit.Tests.Repositorys
-{
-    public class Person
-    {
-        public long Id { get; set; }
-    }
-}
